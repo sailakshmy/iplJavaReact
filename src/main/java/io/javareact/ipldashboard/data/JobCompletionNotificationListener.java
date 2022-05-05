50c5d007d8fb54b9fbfab0e85e6e8269912c18c6
sailakshmy <sailakshmy94@gmail.com>
2022-05-05 23:55:37 +0530

    Commit-6:- Added an entity for the team(Team), added an entitymanager that would help to fetch the details of each team from the Match table in JobCompletionNotificationListener instead of the jdbcTemplate.

--- src/main/java/io/javareact/ipldashboard/data/JobCompletionNotificationListener.java
+++ src/main/java/io/javareact/ipldashboard/data/JobCompletionNotificationListener.java
@@ -3,22 +3,34 @@ package io.javareact.ipldashboard.data;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+
 import org.slf4j.Logger;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import io.javareact.ipldashboard.model.Team;
+
 @Component
 public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
 
     private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
-    private final JdbcTemplate jdbcTemplate;
+    // private final JdbcTemplate jdbcTemplate;
+    private final EntityManager em;
 
     @Autowired
-    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    // public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
+    public JobCompletionNotificationListener(EntityManager em) {
+        // this.jdbcTemplate = jdbcTemplate;
+        this.em = em;
     }
 
     @Override
@@ -26,10 +38,35 @@ public class JobCompletionNotificationListener extends JobExecutionListenerSuppo
         if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
             log.info("Job has been completed. Verify the results.");
 
-            jdbcTemplate.query("SELECT team1, team2, date FROM match",
-                    (rs, row) -> "Team1:- " + rs.getString(1) + " Team2:- " + rs.getString(2)
-                            + " Date:- " + rs.getString(3))
-                    .forEach(str -> System.out.println(str));
+            // jdbcTemplate.query("SELECT team1, team2, date FROM match",
+            // (rs, row) -> "Team1:- " + rs.getString(1) + " Team2:- " + rs.getString(2)
+            // + " Date:- " + rs.getString(3))
+            // .forEach(str -> System.out.println(str));
+
+            // To map the distinct team names to a table
+            Map<String, Team> teamData = new HashMap<>();
+
+            em.createQuery("Select m.team1, count(*) from Match m group by m.team1", Object[].class)
+                    .getResultList()
+                    .stream()
+                    .map(t -> new Team((String) t[0], (long) t[1]))
+                    .forEach(team -> teamData.put(team.getTeamName(), team));
+
+            em.createQuery("Select m.team2, count(*) from Match m group by m.team2", Object[].class)
+                    .getResultList()
+                    .stream()
+                    .forEach(t -> {
+                        Team team = teamData.get((String) t[0]);
+                        team.setTotalMatches(team.getTotalMatches() + (long) t[1]);
+                    });
+
+            em.createQuery("Select m.winningTeam, count(*) from Match m group by m.winningTeam", Object[].class)
+                    .getResultList()
+                    .stream()
+                    .forEach(t -> {
+                        Team team = teamData.get((String) t[0]);
+                        team.setTotalWins((long) t[1]);
+                    });
         }
     }
 }
