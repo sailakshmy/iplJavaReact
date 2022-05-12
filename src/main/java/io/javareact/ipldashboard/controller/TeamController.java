04d3ef3858bca6392e7627f918b0edf212699e95
sailakshmy <sailakshmy94@gmail.com>
2022-05-12 23:54:59 +0530

    Commit-9:- Added the teamrepository reference in the teamController so that the data can be fetched from the persisted DB, Added a default constructor in the Team class as JPA mandates a default constructor.

--- src/main/java/io/javareact/ipldashboard/controller/TeamController.java
+++ src/main/java/io/javareact/ipldashboard/controller/TeamController.java
@@ -5,12 +5,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.javareact.ipldashboard.model.Team;
+import io.javareact.ipldashboard.repository.TeamRepository;
 
 @RestController
 public class TeamController {
 
+    private TeamRepository teamRepository;
+
+    public TeamController(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+    }
+
     @GetMapping("/team/{teamName}")
     public Team getTeam(@PathVariable String teamName) {
-
+        return this.teamRepository.findByTeamName(teamName);
     }
+
 }
