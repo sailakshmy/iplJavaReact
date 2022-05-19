4a80efcb8acaa37141c184f08bea2351c2f39981
sailakshmy <sailakshmy94@gmail.com>
2022-05-19 23:42:56 +0530

    Commit-15:- Created a match page to display the matches played by a team in a year, Added the route in the list of routes.

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import TeamPage from './Pages/TeamPage';
+import MatchPage from './Pages/MatchPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
         <Route exact path='/' element={<h1>IPL Dashboard</h1>} />
         <Route path='/teams/:teamName' exact element={<TeamPage/>} />
+        <Route path='/teams/:teamName/matches/:year' exact element={<MatchPage/>} />
         </Routes>
       </Router>
     </div>
---
+++ src/frontend/src/Pages/MatchPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MatchPage() {
+  return (
+    <div className='matchPage'>
+      <h1>Match Page</h1>
+    </div>
+  )
+}
+
+export default MatchPage
