fabca7ae48feac3ee1c34ca56c5ffb9040efc909
sailakshmy <sailakshmy94@gmail.com>
2022-05-20 00:29:57 +0530

    Commit-16:- Added an endpoint in the TeamController.java that will fetch the details of all the matches that a team has played in a year, Added the method that would fetch the data from the Match table using a query annotation in MatchRepository, Added the API call that would display the data from the table in the MatchPage component, Displayed the data using the dummy component in MatchPage.

--- src/frontend/src/Pages/MatchPage.jsx
+++ src/frontend/src/Pages/MatchPage.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import LatestMatchCard from '../Components/LatestMatchCard';
 
 function MatchPage() {
+
+  const [matches, setMatches] = useState([]);
+  const [error, setError] = useState(false);
+  const {teamName, year} = useParams();  
+
+  useEffect(()=>{
+      const fetchMatchesByTeamInYear = async ()=>{
+          const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+          const data = await response.json();
+          console.log(data);
+          if(data.length === 0)
+          setError(true);
+          else
+          setMatches(data);
+      };
+      fetchMatchesByTeamInYear();
+  },[teamName, year]);
+  if(error)
+    return <h1>No Matches found!! Please check the team Name and the year.</h1>
   return (
     <div className='matchPage'>
       <h1>Match Page</h1>
+      {matches.map((match)=>( <LatestMatchCard match={match} teamName={teamName} key={match.id} />))}
     </div>
   )
 }
--- src/main/java/io/javareact/ipldashboard/controller/TeamController.java
+++ src/main/java/io/javareact/ipldashboard/controller/TeamController.java
@@ -1,10 +1,15 @@
 package io.javareact.ipldashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.javareact.ipldashboard.model.Match;
 import io.javareact.ipldashboard.model.Team;
 import io.javareact.ipldashboard.repository.MatchRepository;
 import io.javareact.ipldashboard.repository.TeamRepository;
@@ -28,4 +33,14 @@ public class TeamController {
         return team;
     }
 
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year) {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        // return
+        // this.matchRepository.getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(teamName,
+        // startDate,
+        // endDate, teamName, startDate, endDate);
+        return this.matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
+    }
 }
